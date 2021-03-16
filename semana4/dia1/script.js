10110f82e19a5c335b1264ae505c4d24824cd287
Daniel Tello <dantello.319@gmail.com>
2021-03-15 20:03:24 -0500

    comenzamos javascript

---
+++ semana4/dia1/script.js
@@ -0,0 +1,1 @@
+alert("Hola Mundo, me llamo Daniel")
---
+++ semana4/dia1/variables/codigo.js
@@ -0,0 +1,16 @@
+//este es un comentario en javascript//
+
+/* Este es un comentario
+    de varias lineas*/
+
+//variable de tipo texto
+    let nombre = "Daniel"
+    let sistemaOperativo = "Windows"
+    console.log(typeof(nombre))
+
+    // alert("mi nombre es " + nombre + " y mi sistema operativo es " + sistemaOperativo)
+    alert(`Mi segundo nombre es ${nombre} y mi sistema operativo es ${sistemaOperativo}`)
+    let edad = 28
+
+    console.log("2" * 3)
+    console.log("1" + 3)
